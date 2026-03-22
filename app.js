e72a98ce6526b134cc5c25eb6bd87a1d4f86605b
2910Shubham <2910viwan@gmail.com>
2026-03-23 00:08:59 +0530

    filter: code

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ function filter(arr, fn){
   if(typeof fn !== "function") throw new Error("fn must be a function")
   let result = []
   for(let i = 0; i<arr.length; i++){
-  if(fn(arr[i],i) == true) { result.push(arr[i]) }
+  if(fn(arr[i],i)) { result.push(arr[i]) }
 }
 return result
 }
-console.log(filter([9,8,7,10,12,11], function(n, i){
+console.log(filter([-2,-1,0,1,2], function(n, i){
     return n+1
 }))
