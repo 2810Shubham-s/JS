cdafb33dfb2ad6b538cfbdbc37804d15a8a9f846
2910Shubham <2910viwan@gmail.com>
2026-03-24 21:57:35 +0530

    3 time try logic

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-function frequency(arr){
-   let obj = {}
-   for(let item of arr){
-     if(obj[item]){
-        obj[item]++
-     }else if(!obj[item]){
-        obj[item] = 1;
-     }
-   }
-   return obj
+// function frequency(arr){
+//    let obj = {}
+//    for(let item of arr){
+//      if(obj[item]){
+//         obj[item]++
+//      }else if(!obj[item]){
+//         obj[item] = 1;
+//      }
+//    }
+//    return obj
+// }
+
+// console.log(frequency([1,3,1,3,3,5,6,8,4,5]))
+
+//three time retry logic
+
+let count = 1;
+async function fetchApi(){
+    const API = "https://jsonplaceholder.typicode.com/todos1/1";
+    let response = await fetch(API)
+    try {
+      
+      if(response.ok){
+        let data = await response.json();
+        console.log(data)
+        return data;
+      }else if(!response.ok && count<3){
+          
+          console.log(count)
+          setTimeout(fetchApi, 1000);
+          
+      }else{
+           console.log("tried 3 times but failed fetching api")
+      }
+    }catch{
+       throw new Error("Error fetching Data")
+    }
 }
 
-console.log(frequency([1,3,1,3,3,5,6,8,4,5]))
+console.log(fetchApi())
