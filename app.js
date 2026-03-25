a207f267702cc598b58613639cc8e1b7943f774b
2910Shubham <2910viwan@gmail.com>
2026-03-25 21:11:19 +0530

    code: cache fucntion

--- app.js
+++ app.js
@@ -39,18 +39,133 @@
 
 // console.log(fetchApi())
 
+// let compose = function(functions){
+//     return function(x){
+//             let comp=x;
+//            for(let i= functions.length -1; i>=0; i--){
+//             comp = functions[i](comp)
+//           //  console.log(comp)
+//            }
+//            return comp
+//         }
+// }
 
-var compose = function(functions) {
+// let fn = compose([x => 10 + x, x => 2*x, x=> x+x, x => x+1])
+// console.log(fn(2))
+
+
+// var once = function(fn) {
+//         let called = false;
     
-    return function(x) {
-        let val = x;
-        console.log(functions.length)
-        for(let i = functions.length - 1; i>0; i--){
-          val = functions[i](val)
-        }
-        return val
-    }
-};
+//     return function(...args){
+        
+//             if(called) return undefined
+//             called = true
+//             return fn(...args)
+        
+//     }
+// };
 
-let fn = compose([x => 10 * x, x => 10 * x, x => 10 * x])
-console.log(fn(4))
+// let fn = once((a) => a["x"])
+// console.log(fn([{"x": 7}],[{"x": 10}],[{"x": 15}]))
+
+//Cache Fucntion
+
+
+function memoize(fn) {
+    
+    return function(...args) {
+     
+       let  memorisedSum = {};
+       let  memorisedfib = {};
+       let  memorisedFactorial = {};
+       if(fn==="sum"){
+          for(let i = 0; i<agrs.length; i++)
+           if(memorisedSum.args[i]){
+            return memorisedSum.args[i]
+           }else{
+            return sum(args[i])
+           }
+     
+        }
+       if(fn==="factorial"){
+          for(let i = 0; i<agrs.length; i++)
+           if(memorisedFactorial.args[i]){
+            return memorisedFactorial.args[i]
+           }else{
+            return factorial(args[i])
+           }
+     
+        }
+       if(fn==="fib"){
+          for(let i = 0; i<agrs.length; i++)
+           if(memorisedfib.args[i]){
+            return memorisedfib.args[i]
+           }else{
+            return fib(args[i])
+           }
+     
+        }
+       let sum = function(a,b){
+            let sum = a+b;
+            let input = [a,b]
+            memorisedSum.input = sum;
+           return sum;
+        }
+       
+        let fib = (n) => {
+            let input = n;
+            let fib = 0;
+            memorisedfib
+            if(n<=1){
+               fib = 1;
+               return 1;
+            }
+            else { fib =  fib(n-1) + fib(n-2) }
+            memorisedfib.input = fib;
+        };
+        let factorial = (n)=>{
+            let input = n;
+            let fact = 0;
+            if(n<=1){
+              fact = 1;
+              return 1;
+
+            }else {
+            fact = factorial(n-1)*n;
+            return factorial(n-1)*n;
+            }
+        }
+        memorisedFactorial.input = fact;
+
+       
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
