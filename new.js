3c0c40eb93835fcdafa80c3d2508969867e6549f
2910Shubham <2910viwan@gmail.com>
2026-03-29 11:13:07 +0530

    Count vowels and consonants in a string

--- new.js
+++ new.js
@@ -8,6 +8,37 @@ function rev(str){
     }
     return revString.join("");
 }
-console.log(rev("Vinod"))
+//console.log(rev("Vinod"));
 
 //Find largest number in an array --> Done
+let findLargest = function(arr){
+    let largest = -Infinity
+   for(let i =0; i< arr.length; i++){
+       if(arr[i]>=largest){
+        largest = arr[i]
+       }
+   }
+
+   return largest
+}
+
+//console.log(findLargest([1,2,5,6,3,4,8,9,5,10,2,13]))
+
+//Count vowels and consonants in a string -->Done
+
+function vowel(str){
+    str = str.toLowerCase()
+    let countVowel= 0;
+    let countConsonents= 0;
+    for(let char of str){
+        if("aeiou".includes(char)){
+             countVowel++
+        }else if(char>='a' && char<="z" ){
+            countConsonents++
+        }
+    }
+   return {countVowel, countConsonents};
+
+}
+
+console.log(vowel("Hello@World!"))
